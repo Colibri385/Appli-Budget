58633b9881ddbab12bb1fd91dcff6e49d4042f7c
Colibri385 <colibri385@orange.fr>
2019-11-19 14:48:32 +0100

    2eme changement

--- app.js
+++ app.js
@@ -2,24 +2,66 @@
 
 var budgetController = (function () {
 
-    
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+         }
+    };
+
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+            
+            // [1,2,3,4,5], next ID = 6
+            // ID = last ID +1 + CREATE new ID
+            ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+
+            // Create new item based on 'inc' or 'exp type
+            if (type === 'exp'){
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+            return newItem;
+        }
+    }
 
 })();
-
 
 // UI CONTROLLER
 
 var UIController = (function () {
 
     var DOMstrings = {
-        inputType : '.add__type',
-        inputDescription : '.add__description',
-        inputValue : '.add__value',
-        inputBtn : '.add__btn'
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
     };
 
     return {
-        getInput: function() {
+        getInput: function () {
             return {
                 // objets : sera inc or exp
                 type: document.querySelector(DOMstrings.inputType).value,
@@ -27,9 +69,9 @@ var UIController = (function () {
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 // récupère le montant
                 value: document.querySelector(DOMstrings.inputValue).value
-            };        
+            };
         },
-        getDOMstrings: function() {
+        getDOMstrings: function () {
             return DOMstrings;
         }
     };
@@ -46,22 +88,22 @@ var controller = (function (budgetCtrl, UICtrl) {
         document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
         document.addEventListener('keypress', function (event) {
-    
+
             if (event.keyCode === 13 || event.which === 13) {
-    
+
                 ctrlAddItem();
             }
-    
+
         });
     };
 
-    
+
 
     var ctrlAddItem = function () {
 
         // 1. Get the field input data (récupère les données entrées)
 
-        var input = UICtrl.getInput();        
+        var input = UICtrl.getInput();
 
         //2. Add the item to the budget controller
 
@@ -72,9 +114,9 @@ var controller = (function (budgetCtrl, UICtrl) {
         //5 . Display the budget on the UI
     };
 
-        return {
-            init : function() {
-            console.log('Application has started'); 
+    return {
+        init: function () {
+            console.log('Application has started');
             setupEventlisteners();
         }
     };
@@ -82,6 +124,3 @@ var controller = (function (budgetCtrl, UICtrl) {
 })(budgetController, UIController);
 
 controller.init();
-
-
-
