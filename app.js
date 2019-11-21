31b3a8b76e6b03ec06da47dd1e03213ed93ef696
Colibri385 <colibri385@orange.fr>
2019-11-21 15:25:19 +0100

    Maj video 81

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ var budgetController = (function () {
         } ,
 
         testing: function () {
-            // console.log(data);
+            console.log(data);
         }
     };
 
@@ -113,6 +113,10 @@ var UIController = (function () {
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
         expensesContainer: '.expenses__list',
+        budgetLabel : '.budget__value',
+        incomeLabel :'.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel:'.budget__expenses--percentage'
     };
 
     return {
@@ -143,7 +147,6 @@ var UIController = (function () {
 
             // replace the placeholder text with some actual data
 
-
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%', obj.description);
             newHtml = newHtml.replace('%value%', obj.value);
@@ -167,6 +170,20 @@ var UIController = (function () {
 
             fieldsArr[0].focus();
 
+        },
+
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            
+     
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            } else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+     
         },
 
         getDOMstrings: function () {
@@ -207,7 +224,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         // 3. Display the budget on the UI
 
-        console.log(budget);
+        UICtrl.displayBudget(budget);
         
     };
 
@@ -246,6 +263,12 @@ var controller = (function (budgetCtrl, UICtrl) {
     return {
         init: function () {
             console.log('Application has started');
+            UICtrl.displayBudget({
+                budget : 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventlisteners();
         }
     };
